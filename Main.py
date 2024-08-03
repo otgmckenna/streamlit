6d5bc0f03cbe6632e499a4a76654afec36995f8f
Oliver Mckenna <79545236+otgmckenna@users.noreply.github.com>
2024-08-03 15:00:36 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -177,12 +177,9 @@ def corps_comparison():
 
 def score_dist():
     import pandas as pd
-    import streamlit as st
     import numpy as np
     import scipy
-    import plotly.express as px
     import plotly.figure_factory as ff
-    from urllib.error import URLError
     
     df = pd.read_csv('scores.csv')
     
